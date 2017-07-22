860b3ea7ff7ae089574af419ea96531e5110ed81
duks <duks@robooot.com>
2017-07-22 09:19:35 +0800

    Initial Commit

---
+++ DrawView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DrawView
+//
+//  Created by aDu on 2017/7/22.
+//  Copyright © 2017年 DuKaiShun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DrawView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DrawView
+//
+//  Created by aDu on 2017/7/22.
+//  Copyright © 2017年 DuKaiShun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
